f495e767a0b10028ba0aaf6008fd803374b02aa2
jinco13 <jinco13@users.noreply.github.com>
2017-11-21 05:54:59 +0900

    added list

--- src/app/controllers/TodoController.php
+++ src/app/controllers/TodoController.php
@@ -4,8 +4,10 @@ class TodoController extends Controller
 {
     public function welcomeAction()
     {
+        $list = $this->db_manager->get('Todo')->fetchAllTodos();
         return $this->render(array(
             '_token' => $this->generateCsrfToken('todo/welcome'),
+            'list' => $list
         ));
     }
 }
