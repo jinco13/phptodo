c8e86f9a5557fb41723c75b6ffa1a7b492d9a5ce
jinco13 <jinco13@users.noreply.github.com>
2017-11-22 19:09:28 +0900

    new create action for todo

--- src/app/controllers/TodoController.php
+++ src/app/controllers/TodoController.php
@@ -6,8 +6,36 @@ class TodoController extends Controller
     {
         $list = $this->db_manager->get('Todo')->fetchAllTodos();
         return $this->render(array(
-            '_token' => $this->generateCsrfToken('todo/welcome'),
             'list' => $list
+        ));
+    }
+
+    public function createAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('todos/new', $token)) {
+            return $this->redirect('/todos/new');
+        }
+
+        $todo = new Todo();
+        $todo->title = $this->request->getPost('title');
+        $todo->completed = $this->request->getPost('completed');
+
+        //// TODO error check
+
+        $this->db_manager->get('Todo')->insert($todo);
+        //return $this->render(array());
+        return $this->redirect('/todos/new');
+    }
+
+    public function newAction()
+    {
+        return $this->render(array(
+            '_token' => $this->generateCsrfToken('todos/new'),
         ));
     }
 }
