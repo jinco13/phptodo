8252435e914efcba2cb8f3c692f57ce17ebb161f
jinco13 <jinco13@users.noreply.github.com>
2017-11-25 06:06:30 +0900

    list details

--- src/app/views/todo/welcome.php
+++ src/app/views/todo/welcome.php
@@ -1,7 +1,11 @@
-<h1>Welcome! This is test</h1>
-
+<h1>Welcome! This is test<?php echo $_SERVER['REMOTE_ADDR'] ?></h1>
+<a href="/todos/new">Add Todo</a><br/>
 <ul>
 <?php foreach($list as $todo): ?>
-<li><?php echo $todo['title']; ?></li>
+<li>
+    <?php if ($todo->completed) { ?><strike><?php } ?>
+    <?php echo $todo->title; ?>(<?php echo $todo->getCreatedDate() ?>)
+    <?php if ($todo->completed) { ?></strike><?php } ?>
+</li>
 <?php endforeach; ?>
 </ul>
