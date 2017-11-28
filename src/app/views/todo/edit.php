9a7fe1750d961c6b6c515613a59fc4919a88e4cd
jinco13 <jinco13@users.noreply.github.com>
2017-11-29 05:55:54 +0900

    edit view

---
+++ src/app/views/todo/edit.php
@@ -0,0 +1,15 @@
+<h1>Edit Todo</h1>
+<form action="/todos/update" method="POST">
+    <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
+    <input type="hidden" name="id" value="<?php echo $todo->id ?>"/>
+    <input type="text" name="title" value="<?php echo $todo->title ?>"/>
+    <input type="checkbox" name="completed" value="TRUE" <?php echo $todo->getCompletedCheck();?>/><br/>
+    <label>Created At: <?php echo $todo->getCreatedDate() ?></label>
+    <br/>
+
+    <input type="submit" value="Update"/>
+</form>
+<form action="/todos/delete" method="POST">
+    <input type="hidden" name="id" value="<?php echo $todo->id ?>"/>
+    <input type="submit" value="Delete"/>
+</form>
